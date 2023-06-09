d61df1c91ee2381b59eb35e919aadc1d10767711
DADJOD <kamu57@mail.ru>
2023-06-09 17:06:10 +0300

    commit_6

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -8,6 +8,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -16,10 +17,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.RemoteInput
+import androidx.core.app.TaskStackBuilder
 
 // import android.support.v7.app.NotificationCompat;
 
 
+@Suppress("UNUSED_PARAMETER")
 class MainActivity : AppCompatActivity() {
 
     private lateinit var manager: NotificationManagerCompat
@@ -66,8 +69,7 @@ class MainActivity : AppCompatActivity() {
             notificationManager.createNotificationChannel(channel)
         }
 
-        val builder = NotificationCompat.Builder(this, channelId)
-        return builder
+        return NotificationCompat.Builder(this, channelId)
     }
 
     // Удаление уведомление -
@@ -121,27 +123,123 @@ class MainActivity : AppCompatActivity() {
     // Сложное уведомление - содержит дополнительные кнопки
     // по щелчку на которые запустятся другие
     // PendingIntent
+
+
     fun complexNotification(view: View?) {
 
+        val notificationManager = getSystemService(NotificationManager::class.java)
+
         // PendingIntent для запуска браузера
+        val pBrowser =
+            Utility.getUriPendingIntent(this, R.id.BROWSER_PENDING_ID, "https://www.louvre.fr/")
 
         // PendingIntent для запуска карты
+        val pMap = Utility.getUriPendingIntent(this, R.id.MAP_PENDING_ID, "geo:48.8606,2.3376")
+        val builder = doBuilder()
+        builder
+            .setSmallIcon(android.R.drawable.ic_input_add)
+            .setContentTitle("Экскурсия в Лувр")
+            .setContentText("Экскурсия начнется через 15 мин")
 
-        // Акция - кнопка с иконкой, текстом и
-        // PendingIntent
+        // Акция - кнопка с иконкой, текстом и PendingIntent
+        // Кнопка для открытия веб-сайта
+        builder.addAction(
+            android.R.drawable.ic_menu_view,
+            "Открыть сайт Лувра",
+            pBrowser
+        )
+
+        // Кнопка для открытия карты
+        builder.addAction(
+            android.R.drawable.ic_menu_mapmode,
+            "Открыть карту Лувра",
+            pMap
+        )
+
+        // Все равно необходим BroadcastReceiver -> onReceive()
+//        builder.addAction(
+//            android.R.drawable.ic_menu_close_clear_cancel,
+//            "Закрыть",
+//            notificationManager.cancel(R.id.LOUVRE_NOTIFICATION_ID)
+//        )
+
+//        builder.setAutoCancel(true)
+//        manager.notify(R.id.LOUVRE_NOTIFICATION_ID, builder.build()) // u need add this -->     @SuppressLint("MissingPermission")
+
+//        builder.setContentIntent(
+//            Utility.getUriPendingIntent(this, R.id.MAIN_ACTIVITY_PENDING_ID, PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_ONE_SHOT)
+//        )
+
+        val notification = builder.build()
+
+        notificationManager.notify(R.id.LOUVRE_NOTIFICATION_ID, notification)
     }
 
-    private val numberOfMessages = 12
+    private var numberOfMessages = 12
 
     // Новый вид уведомлений - "Большая картинка"
     // Будет полноэкранным (приоритет + звук)
     // Со стэком активностей
     fun bigPicture(view: View?) {
 
+        val task = TaskStackBuilder.create(this)
+        task.addNextIntent(Intent(this, MainActivity::class.java))
+        task.addNextIntent(Intent(this, A2::class.java))
+        task.addNextIntent(Intent(this, A3::class.java))
+
+        val pTask = task.getPendingIntent(
+            R.id.TASK_PENDING_ID,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
         // Вначале создадим стиль и определим его
+        val style = NotificationCompat.BigPictureStyle()
 
+        style.setBigContentTitle("Новые сообщения")
+        style.setSummaryText("У вас ${++numberOfMessages} новых сообщений от Ленны")
         // Картинка из ресурсов
+        style.bigPicture(BitmapFactory.decodeResource(resources, R.drawable.lenna))
+        
+        val channelIid = "my_channel_id"
+        val channelName = "Channel for my app"
+        val soundUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.bikeringbell)
+        val defaultVibrationPattern = longArrayOf(500, 200, 500)
+
+        val builder = NotificationCompat.Builder(this, channelIid)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelDescription = "My channel description"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelIid, channelName, importance)
+            channel.description = channelDescription
+            channel.enableVibration(true)
+//            channel.vibrationPattern = defaultVibrationPattern
+            channel.setSound(soundUri, null)
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+            builder.setChannelId(channelIid)
+        }
+
+//        val defaultVibrationPattern = longArrayOf(500, 200,500)
+
+        builder
+            .setSmallIcon(android.R.drawable.btn_radio)
+            .setStyle(style)
+            .setContentIntent(pTask)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVibrate(defaultVibrationPattern)
+            .setSound(soundUri)
+//            .setDefaults(Notification.DEFAULT_ALL)
+
+        val notification = builder.build()
+
+//        manager.notify(R.id.BIG_PICTURE_NOTIFICATION_ID, builder.build())
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(R.id.BIG_PICTURE_NOTIFICATION_ID, notification)
 
         // Есть еще виды стилей
         // MediaStyle - для проигрывания звука или видео
@@ -173,29 +271,82 @@ class MainActivity : AppCompatActivity() {
 
     // Inline reply уведомление -
     // можно ввести текст
-    @SuppressLint("MissingPermission")
-    fun inlineReply(view: View?) {
-        val replyLabel = "Ответ"
-        val remoteInput = RemoteInput.Builder(
-            resources.getString(R.string.KEY_TEXT_REPLY)
-        )
-            .setLabel(replyLabel)
-            .build()
-        val intent: Intent? = null
-        val replyPendingIntent: PendingIntent? = null
-        val replyAction = NotificationCompat.Action.Builder(
-            android.R.drawable.btn_plus,
-            "Ответить",
-            replyPendingIntent
-        )
-            .addRemoteInput(remoteInput)
-            .build()
-        val newMessageNotification: Notification? = null
-        NotificationManagerCompat.from(this).notify(
-            R.id.DIRECT_REPLY_NOTIFICATION_ID,
-            newMessageNotification!!
-        )
-    }
+//    @SuppressLint("MissingPermission")
+//    fun inlineReply(view: View?) {
+//        val replyLabel = "Ответ"
+//        val remoteInput = RemoteInput.Builder(
+//            resources.getString(R.string.KEY_TEXT_REPLY)
+//        )
+//            .setLabel(replyLabel)
+//            .build()
+//        val intent: Intent? = null
+//        val replyPendingIntent: PendingIntent? = null
+//        val replyAction = NotificationCompat.Action.Builder(
+//            android.R.drawable.btn_plus,
+//            "Ответить",
+//            replyPendingIntent
+//        )
+//            .addRemoteInput(remoteInput)
+//            .build()
+//        val newMessageNotification: Notification? = null
+//        NotificationManagerCompat.from(this).notify(
+//            R.id.DIRECT_REPLY_NOTIFICATION_ID,
+//            newMessageNotification!!
+//        )
+//    }
+
+
+//    fun complexNotification(view: View?) {
+//
+////        // PendingIntent для закрытия уведомления
+////        val pClose = Utility.getBroadcastPendingIntent(this, R.id.CLOSE_PENDING_ID, Intent(this, NotificationReceiver::class.java).apply {
+////            action = NotificationReceiver.ACTION_CLOSE_NOTIFICATION
+////        }, PendingIntent.FLAG_ONE_SHOT)
+//
+//        // PendingIntent для запуска браузера
+//        val pBrowser = Utility.getUriPendingIntent(this, R.id.BROWSER_PENDING_ID, "https://www.louvre.fr/", PendingIntent.FLAG_ONE_SHOT, R.id.LOUVRE_NOTIFICATION_ID)
+//
+//// PendingIntent для запуска карты
+//        val pMap = Utility.getUriPendingIntent(this, R.id.MAP_PENDING_ID, "geo:48.8606,2.3376", PendingIntent.FLAG_ONE_SHOT, R.id.LOUVRE_NOTIFICATION_ID)
+////        // PendingIntent для запуска браузера
+////        val pBrowser = Utility.getUriPendingIntent(this, R.id.BROWSER_PENDING_ID, "https://www.louvre.fr/", PendingIntent.FLAG_ONE_SHOT)
+////
+////        // PendingIntent для запуска карты
+////        val pMap = Utility.getUriPendingIntent(this, R.id.MAP_PENDING_ID, "geo:48.8606,2.3376", PendingIntent.FLAG_ONE_SHOT)
+//
+//        val builder = doBuilder()
+//        builder
+//            .setSmallIcon(android.R.drawable.ic_input_add)
+//            .setContentTitle("Экскурсия в Лувр")
+//            .setContentText("Экскурсия начнется через 15 мин")
+//
+//        // Акция - кнопка с иконкой, текстом и PendingIntent
+//        // Кнопка для открытия веб-сайта
+//        builder.addAction(
+//            android.R.drawable.ic_menu_view,
+//            "Открыть сайт Лувра",
+//            pBrowser
+//        )
+//
+//        // Кнопка для открытия карты
+//        builder.addAction(
+//            android.R.drawable.ic_menu_mapmode,
+//            "Открыть карту Лувра",
+//            pMap
+//        )
+//
+//        // Кнопка для закрытия уведомления
+////        builder.addAction(
+////            android.R.drawable.ic_menu_close_clear_cancel,
+////            "Закрыть",
+////            pClose
+////        )
+//
+//        val notification = builder.build()
+//
+//        val notificationManager = getSystemService(NotificationManager::class.java)
+//        notificationManager.notify(R.id.LOUVRE_NOTIFICATION_ID, notification)
+//    }
 }
 
 
--- app/src/main/java/com/example/notification/Utility.kt
+++ app/src/main/java/com/example/notification/Utility.kt
@@ -7,7 +7,12 @@ import android.net.Uri
 import androidx.core.app.TaskStackBuilder
 
 internal object Utility {
-    fun getUriPendingIntent(context: Context, pendingIntentId: Int, uri: String?): PendingIntent {
+
+    fun getUriPendingIntent(
+        context: Context,
+        pendingIntentId: Int,
+        uri: String,
+    ): PendingIntent {
         val intent = Intent()
         intent.action = Intent.ACTION_VIEW
         intent.data = Uri.parse(uri)
@@ -32,4 +37,73 @@ internal object Utility {
             PendingIntent.FLAG_UPDATE_CURRENT
         )
     }
+
+
+//    fun getUriPendingIntent(context: Context, requestCode: Int, uriString: String, flags: Int): PendingIntent {
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uriString))
+//        return PendingIntent.getActivity(context, requestCode, intent, flags)
+//    }
+//
+//    fun getBroadcastPendingIntent(context: Context, requestCode: Int, intent: Intent, flags: Int): PendingIntent {
+//        return PendingIntent.getBroadcast(context, requestCode, intent, flags)
+//    }
+
+//    fun getUriPendingIntentWithFlags(context: Context, requestCode: Int, uri: String, flags: Int): PendingIntent {
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+//        return PendingIntent.getActivity(context, requestCode, intent, flags)
+//    }
+
+//    fun getMainActivityPendingIntent(context: Context, flags: Int): PendingIntent {
+//        val intent = Intent(context, MainActivity::class.java)
+//        return PendingIntent.getActivity(context, 0, intent, flags)
+//    }
+
+
+//    @SuppressLint("UnspecifiedImmutableFlag")
+//    fun getUriPendingIntent(context: Context, requestCode: Int, uri: String, flags: Int, notificationId: Int): PendingIntent {
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+//
+//        // Создаем PendingIntent для запуска действия
+//        val pendingIntent = PendingIntent.getActivity(
+//            context,
+//            requestCode,
+//            intent,
+//            PendingIntent.FLAG_CANCEL_CURRENT
+//        )
+//
+//        // Создаем BroadcastReceiver для передачи действия в будущем
+//        val broadcastReceiverIntent = Intent(context, NotificationReceiver::class.java).apply {
+//            action = NotificationReceiver.ACTION_DISMISS_NOTIFICATION // установить действие "отмены" уведомления
+//            putExtra(EXTRA_NOTIFICATION_ID, notificationId) // установить идентификатор уведомления
+//        }
+//
+//        // Создаем PendingIntent для "отмены" уведомления
+//        val dismissPendingIntent = PendingIntent.getBroadcast(
+//            context,
+//            requestCode,
+//            broadcastReceiverIntent,
+//            PendingIntent.FLAG_ONE_SHOT
+//        )
+//
+//        // Создаем IntentFilter для фильтрации событий "отмены" уведомлений
+//        val intentFilter = IntentFilter(NotificationReceiver.ACTION_DISMISS_NOTIFICATION)
+//
+//        // Регистрируем BroadcastReceiver для получения событий "отмены" уведомлений
+//        context.registerReceiver(object : BroadcastReceiver() {
+//            override fun onReceive(context: Context, intent: Intent) {
+//                val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//                val notificationId = intent.getIntExtra(EXTRA_NOTIFICATION_ID, 0)
+//                notificationManager.cancel(notificationId)
+//            }
+//        }, intentFilter)
+//
+//        // Объединяем оба действия в одно PendingIntent
+//        val actions: Array<PendingIntent> = arrayOf(dismissPendingIntent, pendingIntent)
+//        return PendingIntent.getActivities(
+//            context,
+//            requestCode,
+//            actions,
+//            flags
+//        )
+//    }
 }
